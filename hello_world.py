190418b5e69f4bf7453ca4cbad237963854fce5f
Gbolahan Adeleke <66179522+adelekeofficail@users.noreply.github.com>
2023-12-13 13:00:33 +0000

    hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+import datetime
+
+def main():
+    # Print "Hello, World!"
+    print("Hello, World!")
+
+    # Get the current time and print it
+    current_time = datetime.datetime.now()
+    print("Current time:", current_time)
+
+if __name__ == "__main__":
+    main()
